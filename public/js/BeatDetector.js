c2c3ef44d7c187572fb3eafad3698c836e3ea973
Stefan Hedman <schteppe@gmail.com>
2014-10-16 20:56:56 +0200

    better beat detector

--- public/js/BeatDetector.js
+++ public/js/BeatDetector.js
@@ -10,6 +10,10 @@ DANSA.BeatDetector.prototype.reset = function(){
 };
 
 DANSA.BeatDetector.prototype.addQuarter = function(time){
+	if(time < this.timeValues[this.timeValues.length - 1]){
+		this.reset();
+	}
+
 	this.timeValues.push(time);
 
 	// Update bpm
@@ -18,8 +22,7 @@ DANSA.BeatDetector.prototype.addQuarter = function(time){
 		var t0 = this.timeValues[i - 1] - this.timeValues[0];
 		var t1 = this.timeValues[i] - this.timeValues[0];
 		var bps = 1 / (t1 - t0);
-		bpsSum += bps;
 	}
-	this.bps = bpsSum / (this.timeValues.length - 1);
+	this.bps = (this.timeValues.length - 1) / (time - this.timeValues[0]);
 	this.bpm = this.bps * 60;
 };
